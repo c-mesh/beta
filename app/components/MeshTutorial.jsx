cdb1fd63ce0f1f4d78195722630d0f0133011755
madeyinka@terragonltd.com <madeyinka@terragonltd.com>
2018-05-05 18:47:23 +0100

    how you appear page done

--- app/components/MeshTutorial.jsx
+++ app/components/MeshTutorial.jsx
@@ -8,28 +8,23 @@ class MeshTutorial extends Component {
         return(
             <div className="mesh-tutorial">
                 <div className="title row col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                    <h2 style={{fontStyle:"italic", marginTop: 0}}>Quick tutorial:</h2>
+                        <h2 style={{fontStyle:"italic", marginTop: 0}}>How you appear:</h2>
                     <hr/>
                 </div>
                 <div className="tutorial">
                     <div style={{padding:0}} className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                        <div className="img-container">
-                            <img className="img img-responsive"
-                                src={baseUrl+"assets/images/mesh_tutorial_1.png"}/>
-                        </div>
-                        <div className="mesh-panel">
-                            {this.props.meshPanel}
-                        </div>
                         <div className="linkedInfo">
                             {this.props.linkedinPanel}
                         </div>
                     </div>
                 </div>
                 <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
-                    <hr/>
-                    <a className="btn btn-primary got-it-btn" onClick={this.props.onGotIt}>
-                        <span>Got it!</span>
-                    </a>
+                    <div className="got-it">
+                        <hr/>
+                        <a className="btn btn-primary got-it-btn" onClick={this.props.onGotIt}>
+                            <span>Got it!</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
